7e65cf2f3d45cfc838cce9ba91937c90366b1441
Joe Baker <jbaker0428@gmail.com>
2011-11-17 01:38:42 -0500

    Added the rest of the registers for the L3G4200D.

--- XMOS_Code/URDB_libXMOS/include/L3G4200D.h
+++ XMOS_Code/URDB_libXMOS/include/L3G4200D.h
@@ -82,5 +82,63 @@
 #define CTRL_REG5_OUT_SEL1	1 << 1
 #define CTRL_REG5_OUT_SEL0	1
 
+// STATUS_REG Register
+#define STATUS_REG_ZYXOR	1 << 7
+#define STATUS_REG_ZOR		1 << 6
+#define STATUS_REG_YOR		1 << 5
+#define STATUS_REG_XOR		1 << 4
+#define STATUS_REG_ZYXDA	1 << 3
+#define STATUS_REG_ZDA		1 << 2
+#define STATUS_REG_YDA		1 << 1
+#define STATUS_REG_XDA		1
+
+// FIFO_CTRL_REG Register
+#define FIFO_CTRL_REG_FM2	1 << 7
+#define FIFO_CTRL_REG_FM1	1 << 6
+#define FIFO_CTRL_REG_FM0	1 << 5
+#define FIFO_CTRL_REG_WTM4	1 << 4
+#define FIFO_CTRL_REG_WTM3	1 << 3
+#define FIFO_CTRL_REG_WTM2	1 << 2
+#define FIFO_CTRL_REG_WTM1	1 << 1
+#define FIFO_CTRL_REG_WTM0	1
+
+// FIFO_SRC_REG Register
+#define FIFO_SRC_REG_WTM	1 << 7
+#define FIFO_SRC_REG_OVRN	1 << 6
+#define FIFO_SRC_REG_EMPTY	1 << 5
+#define FIFO_SRC_REG_FSS4	1 << 4
+#define FIFO_SRC_REG_FSS3	1 << 3
+#define FIFO_SRC_REG_FSS2	1 << 2
+#define FIFO_SRC_REG_FSS1	1 << 1
+#define FIFO_SRC_REG_FSS0	1
+
+// INT1_CFG Register
+#define INT1_CFG_ANDOR		1 << 7
+#define INT1_CFG_LIR		1 << 6
+#define INT1_CFG_ZHIE		1 << 5
+#define INT1_CFG_ZLIE		1 << 4
+#define INT1_CFG_YHIE		1 << 3
+#define INT1_CFG_YLIE		1 << 2
+#define INT1_CFG_XHIE		1 << 1
+#define INT1_CFG_XLIE		1
+
+// INT1_SRC Register
+#define INT1_SRC_IA			1 << 6
+#define INT1_SRC_ZH			1 << 5
+#define INT1_SRC_ZL			1 << 4
+#define INT1_SRC_YH			1 << 3
+#define INT1_SRC_YL			1 << 2
+#define INT1_SRC_XH			1 << 1
+#define INT1_SRC_XL			1
+
+// INT1_DURATION Register
+#define INT1_DURATION_WAIT	1 << 7
+#define INT1_DURATION_D6	1 << 6
+#define INT1_DURATION_D5	1 << 5
+#define INT1_DURATION_D4	1 << 4
+#define INT1_DURATION_D3	1 << 3
+#define INT1_DURATION_D2	1 << 2
+#define INT1_DURATION_D1	1 << 1
+#define INT1_DURATION_D0	1
 
 #endif /* L3G4200D_H_ */
