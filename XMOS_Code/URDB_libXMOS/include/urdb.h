5ac89c5f48f263f7dfd901df9d28e4b1235f325c
Joe Baker <jbaker0428@gmail.com>
2011-11-17 01:57:05 -0500

    Added core definitions to urdb.h.

--- XMOS_Code/URDB_libXMOS/include/urdb.h
+++ XMOS_Code/URDB_libXMOS/include/urdb.h
@@ -17,4 +17,15 @@
 
 #define CLK_REF	100000000	// TODO : This may not be the correct value
 
+#define GADGETEER_CORE	0
+#define	PMOD_O_CORE		0
+#define	PMOD_1_CORE		0
+#define	SERVO_CORE		0
+#define	STM32_CORE		0
+
+#define I2C_CORE		1
+#define SPI_CORE		1
+#define MOTOR_CORE		1
+#define PMOD_2_CORE		1
+
 #endif /* URDB_H_ */
