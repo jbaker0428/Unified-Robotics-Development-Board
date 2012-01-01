6bb31d010688b7426924d4f9208b424643151ccc
Joe Baker <jbaker0428@gmail.com>
2012-01-01 06:48:15 -0500

    Removed volatile declaration from ASM statements (not supported by current XC implementation) and cleaned up formatting.

--- XMOS_Code/sc_urdb/module_urdb/include/xlog_support.h
+++ XMOS_Code/sc_urdb/module_urdb/include/xlog_support.h
@@ -23,73 +23,55 @@
 
 static inline unsigned int _inToken(unsigned int resource_id) {
   unsigned int ret;
-  __asm volatile (
-         "int %0,res[%1] ":"=r"(ret):"r"(resource_id)
-         );
+  asm ("int %0,res[%1] ":"=r"(ret):"r"(resource_id));
   return ret;
 }
 
 
 static inline unsigned int _inCT(unsigned int resource_id) {
   unsigned int ret;
-  __asm volatile (
-         "inct %0,res[%1] ":"=r"(ret):"r"(resource_id)         
-         );
+  asm ("inct %0,res[%1] ":"=r"(ret):"r"(resource_id));
   return ret;
 }
 
 static inline unsigned int _inInt(unsigned int resource_id) {
   unsigned int ret;
-  __asm volatile (
-         "in %0,res[%1] ":"=r"(ret):"r"(resource_id)         
-         );
+  asm ("in %0,res[%1] ":"=r"(ret):"r"(resource_id));
   return ret;
 }
 
 
 static inline void _outCT(unsigned int resource_id, unsigned int token) {
-  __asm volatile (
-                  "outct res[%0],%1" ::"r"(resource_id),"r"(token)
-         );
+  asm ("outct res[%0],%1" ::"r"(resource_id),"r"(token));
   return;
 }
 
 static inline void _outDT(unsigned int resource_id, unsigned int token) {
-  __asm volatile (
-                  "outt res[%0],%1" ::"r"(resource_id),"r"(token)
-         );
+  asm ("outt res[%0],%1" ::"r"(resource_id),"r"(token));
   return;
 }
 
 
 static inline void _outInt(unsigned int resource_id, unsigned int data) {
-  __asm volatile (
-                  "out res[%0],%1" ::"r"(resource_id),"r"(data)
-         );
+  asm ("out res[%0],%1" ::"r"(resource_id),"r"(data));
   return;
 }
 
 
 static inline void _setChanEndDest(unsigned int resource_id, unsigned int dest) {
-  __asm volatile (
-                  "setd res[%0],%1" ::"r"(resource_id),"r"(dest)
-         );
+  asm ("setd res[%0],%1" ::"r"(resource_id),"r"(dest));
   return;
 }
 
 static inline unsigned int _getChanEnd(void) {
   unsigned int ret;
-  __asm volatile (
-     "getr %0, 2" QUOTEME(XS1_RES_TYPE_CHANEND):"=r"(ret)
-         );
+  asm ("getr %0, 2" :"=r"(ret) :);
   return ret;
 }
 
 
 static inline void _freeChanEnd(unsigned int resource_id) {
-  __asm volatile (
-         "freer res[%0] "::"r"(resource_id)         
-         );
+  asm ("freer res[%0] "::"r"(resource_id));
   return;
 }
 
