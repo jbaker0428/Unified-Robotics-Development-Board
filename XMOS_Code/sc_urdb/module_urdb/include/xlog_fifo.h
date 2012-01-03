45fbe1506ad145d0ad0a6f94b63a1406458402e4
Joe Baker <jbaker0428@gmail.com>
2012-01-03 08:42:09 -0500

    Client-side request tracking FIFO.

--- XMOS_Code/sc_urdb/module_urdb/include/xlog_fifo.h
+++ XMOS_Code/sc_urdb/module_urdb/include/xlog_fifo.h
@@ -29,6 +29,14 @@ typedef struct request_fifo_t {
   int request_fifo[XLOG_REQUEST_FIFO_LEN];
 } request_fifo_t;
 
+typedef struct client_fifo_t {
+	int rdIndex;
+	int wrIndex;
+	int data_fifo[XLOG_REQUEST_FIFO_LEN];
+	unsigned char length_fifo[XLOG_REQUEST_FIFO_LEN];
+	REFERENCE_PARAM(int, return_fifo[XLOG_REQUEST_FIFO_LEN]);	// Reference to return data var
+};
+
 #define isempty(x) (x.rdIndex == x.wrIndex)
 
 void msg_fifo_init(REFERENCE_PARAM(msg_fifo_t, x));
@@ -38,5 +46,9 @@ char  msg_fifo_pull(REFERENCE_PARAM(msg_fifo_t, x));
 void request_fifo_init(REFERENCE_PARAM(request_fifo_t, x));
 int request_fifo_push(REFERENCE_PARAM(request_fifo_t, x), unsigned int d);
 unsigned int  request_fifo_pull(REFERENCE_PARAM(request_fifo_t, x));
+
+void client_fifo_init(REFERENCE_PARAM(request_fifo_t, x));
+void client_fifo_push(REFERENCE_PARAM(request_fifo_t, x), unsigned int data, unsigned char length_token, REFERENCE_PARAM(int, ret_ref));
+void client_fifo_pull(REFERENCE_PARAM(request_fifo_t, x), REFERENCE_PARAM(unsigned int, data), REFERENCE_PARAM(unsigned char, length_token), REFERENCE_PARAM(int, ret_ref));
 
 #endif //_xlog_fifo_h_
