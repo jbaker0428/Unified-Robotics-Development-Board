d0f743452c008c84350f880da967fbcb1c110085
Joe Baker <jbaker0428@gmail.com>
2012-01-12 21:48:59 -0500

    Restructuring of client request FIFO.
    This change introduces another struct type, io_request_t. An IO request object contains the request's data, data length, return reference, and a status flag indicating if it has been serviced.
    The client_fifo_t type now holds an array of io_request_t references.

--- XMOS_Code/sc_urdb/module_urdb/include/io_servers.h
+++ XMOS_Code/sc_urdb/module_urdb/include/io_servers.h
@@ -40,18 +40,16 @@ void spi_server(unsigned &server);
 void stm32_uart_server(unsigned &server);
 
 /**
- * @fn unsigned int io_client_enqueue(unsigned &server, client_fifo_t &client_fifo, int data, int data_len, REFERENCE_PARAM(int, ret_data))
+ * @fn unsigned int io_client_enqueue(unsigned &server, unsigned &client, client_fifo_t &client_fifo, int data, int data_len, REFERENCE_PARAM(int, ret_data))
  * @brief Sends a request to be added to a server's queue
  * @param server Reference to variable holding the destination server's resource ID
  * @param client Reference to variable holding the client's chanend resource ID
  * @param client_fifo Client's IO request bookkeeping FIFO
- * @param data Data to be sent over the server's IO bus
- * @param data_len Number of words (based on IO bus word length) to be sent
- * @param ret_data Reference to variable that will hold any returned data over the IO bus
+ * @param request Structure of type io_request_t containing IO request information
  * @return CT_ACK/CT_NACK token value returned by server
  */
 unsigned int io_client_enqueue(unsigned &server, unsigned &client, client_fifo_t &client_fifo,
-		int data, int data_len, REFERENCE_PARAM(int, ret_data));
+		io_request_t &request);
 
 #ifdef __cplusplus
 }
