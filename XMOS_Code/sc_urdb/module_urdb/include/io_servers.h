25802061dd2cbfc625e0473535f2552b7b52d88c
Joe Baker <jbaker0428@gmail.com>
2012-01-12 21:07:09 -0500

    Prototype for client function to send a service request to an IO server.

--- XMOS_Code/sc_urdb/module_urdb/include/io_servers.h
+++ XMOS_Code/sc_urdb/module_urdb/include/io_servers.h
@@ -39,6 +39,20 @@ void spi_server(unsigned &server);
  */
 void stm32_uart_server(unsigned &server);
 
+/**
+ * @fn unsigned int io_client_enqueue(unsigned &server, client_fifo_t &client_fifo, int data, int data_len, REFERENCE_PARAM(int, ret_data))
+ * @brief Sends a request to be added to a server's queue
+ * @param server Reference to variable holding the destination server's resource ID
+ * @param client Reference to variable holding the client's chanend resource ID
+ * @param client_fifo Client's IO request bookkeeping FIFO
+ * @param data Data to be sent over the server's IO bus
+ * @param data_len Number of words (based on IO bus word length) to be sent
+ * @param ret_data Reference to variable that will hold any returned data over the IO bus
+ * @return CT_ACK/CT_NACK token value returned by server
+ */
+unsigned int io_client_enqueue(unsigned &server, unsigned &client, client_fifo_t &client_fifo,
+		int data, int data_len, REFERENCE_PARAM(int, ret_data));
+
 #ifdef __cplusplus
 }
 #endif
